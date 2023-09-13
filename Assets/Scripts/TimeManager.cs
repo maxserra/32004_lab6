5445b39a8fba7482a99eb14bd03aef09c4b517c0
Max Serra <mserralasierra@gmail.com>
2023-09-13 21:01:25 +1000

    80 percent complete

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -12,6 +12,16 @@ public class TimeManager : MonoBehaviour
 
     private const float moveWait = 2.0f;
 
+    private float max_x;
+    private float min_x;
+    private float max_y;
+    private float min_y;
+
+    private Vector3 topRight;
+    private Vector3 botRight;
+    private Vector3 botLeft;
+    private Vector3 topLeft;
+
     private void Start()
     {
         // Reset time
@@ -31,6 +41,13 @@ public class TimeManager : MonoBehaviour
         {
             Debug.Log((int)timer);
             lastTime = (int)timer;
+
+            // Move objects
+            if ((int)timer % moveWait == 0)
+            {
+                Debug.Log("Moving objects...");
+                MoveObjects();
+            }
         }
         timer += Time.deltaTime;
 
@@ -49,11 +66,40 @@ public class TimeManager : MonoBehaviour
         {
             ResetTime();
         }
+
+
     }
 
     private void MoveObjects()
     {
+        max_x = Mathf.Max(transformArray[0].position.x,
+                              transformArray[1].position.x);
+        min_x = Mathf.Min(transformArray[0].position.x,
+                          transformArray[1].position.x);
+        max_y = Mathf.Max(transformArray[0].position.y,
+                          transformArray[1].position.y);
+        min_y = Mathf.Min(transformArray[0].position.y,
+                          transformArray[1].position.y);
 
+        topRight = new Vector3(max_x, max_y, 0);
+        botRight = new Vector3(max_x, min_y, 0);
+        botLeft = new Vector3(min_x, min_y, 0);
+        topLeft = new Vector3(min_x, max_y, 0);
+
+        MoveOneObjectClockwise(transformArray[0]);
+        MoveOneObjectClockwise(transformArray[1]);
+    }
+
+    private void MoveOneObjectClockwise(Transform objTransform)
+    {
+        if (objTransform.position == topRight)
+            objTransform.position = botRight;
+        else if (objTransform.position == botRight)
+            objTransform.position = botLeft;
+        else if (objTransform.position == botLeft)
+            objTransform.position = topLeft;
+        else if (objTransform.position == topLeft)
+            objTransform.position = topRight;
     }
 
     private void ResetTime()
