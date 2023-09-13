305f03e1c0e053f560775f87967ef1f8395dad5e
Max Serra <mserralasierra@gmail.com>
2023-09-13 19:42:33 +1000

    50 percent complete

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -3,7 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
-{ 
+{
 
-    
+    private int lastTime = 0;
+
+    private void Start()
+    {
+        Debug.Log(lastTime);
+    }
+
+    private void Update()
+    {
+        if ((Time.time - lastTime) >= 1)
+        {
+            lastTime = (int)Time.time;
+            Debug.Log(lastTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Spacebar pressed");
+            if (Time.timeScale != 0)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+    }
+
 }
